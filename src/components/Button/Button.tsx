09d5639cbb1ec902d21078f7e74c60adec5b8e25
anischenko alexander <a.anischenko.de@gmail.com>
2024-11-28 15:55:03 +0100

    HomeWork07 update

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,10 @@ import { ButtonProps } from "./types";
 import { MainButton } from "./styles.ts";
 
 //props ='значение по умолчанию(default)'
-function Button({ buttonName, type = "button", onClick, }: ButtonProps,) {
+function Button({ buttonName, type = "button", onClick, disabled}: ButtonProps,) {
     //console.log(props);
 
-    return <MainButton onClick={onClick} >{buttonName}</MainButton>;
+    return <MainButton onClick={onClick} disabled = {disabled}>{buttonName}</MainButton>;
 }
 
 export default Button;
