54503cace69ef1a0e2c03586aa5179c45349fc6d
anischenko alexander <a.anischenko.de@gmail.com>
2024-12-05 19:41:45 +0100

    Lesson10 HomeWork09

---
+++ src/homeworks/Homework09/Homework09.tsx
@@ -0,0 +1,68 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import axios from "axios";
+import { DogImg, ErrorMessage, Homework09Component, Spinner } from "./styles";
+import Input from "../../components/Input/Input";
+
+function Homework09() {
+    const [search, setSearch] = useState<string>("");
+    const [note, setNote] = useState<string>("");
+    const [dogImageUrl, setDogImageUrl] = useState<string | undefined>(
+        undefined
+    );
+    const [error, setError] = useState<any>(undefined);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+    const onSearchCange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+    };
+
+    const onNoteCange = (event: ChangeEvent<HTMLInputElement>) => {
+        setNote(event.target.value);
+    };
+
+    const getDogPhoto = async () => {
+        setDogImageUrl(undefined)
+        setError(undefined)
+        try {
+            setIsLoading(true)
+            const response = await axios.get(DOG_URL);
+            /* const response = await axios.gpost(url, {
+                name: 'Bob',
+                email: 'bob@gmail.com'
+            }); */
+            //console.log(response);
+            setDogImageUrl(response.data.message)
+        } catch (error: any) {
+            //console.log(error);
+        } finally{
+            setIsLoading(false)
+        }
+    };
+
+    useEffect(() => {
+        getDogPhoto()
+    },[search])
+
+    return (
+        <Homework09Component>
+            <Input
+                name="search"
+                placeholder="Enter word"
+                value={search}
+                onChange={onSearchCange}
+            />
+            <Input
+                name="note"
+                placeholder="Enter note"
+                value={note}
+                onChange={onNoteCange}
+            />
+            {dogImageUrl && <DogImg src={dogImageUrl} alt="random dog" />}
+            {error && <ErrorMessage>{error.mesage}</ErrorMessage>}
+            {isLoading && <Spinner></Spinner>}
+        </Homework09Component>
+    );
+}
+export default Homework09;
